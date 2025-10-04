531b86b2eeccdef7adadf39c53b66ac096bb4dc3
Leonardo Scotti <scotti.leonardo25@gmail.com>
2025-10-04 18:42:09 +0200

    Update routers.py

---
+++ routers.py
@@ -0,0 +1,68 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import HTTPException
+from pydantic import BaseModel
+from datetime import datetime, timedelta
+from fastapi import FastAPI
+from fastapi import Depends
+from fastapi import status
+import random
+import asyncio
+
+#custom lib imports
+import api_calls as api
+import sim
+
+#startup
+
+app = FastAPI(
+    title="QuackSpaceMeteorMadness",
+    version="0.1"
+)
+
+status = 1;
+r_count = 0;
+startup_time = datetime.now()
+
+async_results = {}
+
+####GET REQUESTS####
+
+@app.get("/")
+def root():
+    uptime = timedelta(datetime.now(), startup_time)
+    return {"status": status, "r_count": r_count, "uptime": uptime}
+
+@app.get("/asteroids/")
+def asteroids_catalog(start: int, offset: int):
+    catalog = api.NEO_catalog(start, offset)
+    return  {"asteroids": catalog}
+
+@app.get("/asteroids/id/")
+def asteroid_data(obj_id: str):
+    data = api.NEO_by_id(obj_id)
+    return {"data": data}
+
+@app.get("/simulate/")
+async def full_sim(obj_id: str, preview: bool):
+    if preview:
+        data = api.NEO_by_id(obj_id, job_id=None)
+        result = sim.full(data)
+        return result
+    else:
+        job_id = random.randint(00000, 99999)
+        asyncio.create_task(sim(data, job_id))
+        return {"job_id": job_id}
+
+@app.get("simulate/results/{job_id}")
+def get_results(job_id: int):
+    result = async_results[job_id]["data"]
+    status = async_results[job_id]["status"]
+    return {"status": status, "result": result}
+
+####POST REQUESTS####
+
+@app.post("/simulate-impact")
+def impact_sim(data: dict):
+    result = sim.impact(data)
+    return result
+    
